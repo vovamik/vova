5b9e055a15d3e6d5e0d4f12799deb080f1c9d6b5
Владимир <@192.168.4.100>
2014-09-30 18:10:41 +0300

    Фиксация

---
+++ PhpProject1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+ffff
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
